d0c8da2bf793fd6eebf5dd99fe38495e4ce608ea
Broguypal <145296607+Broguypal@users.noreply.github.com>
2025-09-05 14:10:31 -0600

    Add files via upload
    Added Aminon elements

--- tTracker/Monster_Ability_Elements.lua
+++ tTracker/Monster_Ability_Elements.lua
@@ -3,23 +3,31 @@
 return {
     water = {
         ["Flashflood"] = true,
+        ["Torrential Pain"] = true,
     },
     wind = {
+        ["Blast of Reticence"] = true,
     },
     thunder = {
         ["Fulminous Smash"] = true,
+        ["Ceaseless Surge"] = true,
     },
     ice = {
         ["Icy Grasp"] = true,
+        ["Frozen Blood"] = true,
     },
     earth = {
         ["Eroding Flesh"] = true,
+        ["Ensepulcher"] = true,
     },
     fire = {
+        ["Demonfire"] = true,
         ["Flaming Kick"] = true,
     },
     light = {
     },
     dark = {
+        ["Black Cloud"] = true,
+        ["Blood Saber"] = true,
     },
 }
