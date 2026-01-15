462383f2d83c69399c53b9f23d41f87ab7b31b63
Broguypal <145296607+Broguypal@users.noreply.github.com>
2026-01-14 22:16:05 -0700

    Update TrueTargetLock.lua

--- TrueTargetLock/TrueTargetLock.lua
+++ TrueTargetLock/TrueTargetLock.lua
@@ -1,3 +1,6 @@
+-- License: BSD 3-Clause (see bottom of file)
+-- Copyright (c) 2026 Broguypal
+
 _addon.name    = 'TrueTargetLock'
 _addon.author  = 'Broguypal'
 _addon.version = '1.1'
@@ -118,3 +121,4 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
+
