bef76ef422f33e960e745a70f2cca39e0619ff8e
Broguypal <145296607+Broguypal@users.noreply.github.com>
2026-01-14 21:20:27 -0700

    Add files via upload

--- tTracker/tTracker.lua
+++ tTracker/tTracker.lua
@@ -83,7 +83,7 @@ local defaults = {
     timeout = 20
 }
 
--- Save settings helper 
+-- Save settings helper
 local function save_settings()
     if not settings or not output_box then return end
     local x, y = output_box:pos()
@@ -559,5 +559,28 @@ end)
 windower.register_event('unload', save_settings)
 
 -- Also save on logout
-
 windower.register_event('logout', save_settings)
+
+--[[
+MIT License
+
+Copyright (c) 2026 Broguypal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
