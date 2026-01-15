c25b19f4d73025d17d000a55cbb4f8ce6f23208c
Broguypal <145296607+Broguypal@users.noreply.github.com>
2026-01-14 22:16:26 -0700

    Update AbysseaProc.lua

--- AbysseaProc/AbysseaProc.lua
+++ AbysseaProc/AbysseaProc.lua
@@ -1,3 +1,6 @@
+-- License: BSD 3-Clause (see bottom of file)
+-- Copyright (c) 2026 Broguypal
+
 -- AbysseaProc - Element / Weapon / WS cycler with HUD
 
 _addon.name     = 'AbysseaProc'
@@ -325,3 +328,4 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
+
